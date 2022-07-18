e041194f55258c3e6ed1405b1fbacf1a2d7d14cb
nugrohoraharjo <nugrohor85@gmail.com>
2022-07-18 16:17:45 +0700

    My Django Girls app,Add Comment

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.utils import timezone
 # Create your views here.
@@ -11,7 +11,23 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    
+
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.date = timezone.now()
+            comment.save()
+            # return redirect('post_detail', pk=post.pk)
+    else:
+        comment_form = CommentForm()
+    comments = post.comments.all()
+    return render(request, 'blog/post_detail.html', {'post': post,'comments': comments, 'comment_form': comment_form })
+
+
+    
 
 def post_new(request):
     if request.method == "POST":
@@ -39,3 +55,17 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def comment_new(request,pk):
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            # comment.user = request.user
+            comment.post = get_object_or_404(Post, pk=pk)
+            comment.date = timezone.now()
+            # comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'form': form})
